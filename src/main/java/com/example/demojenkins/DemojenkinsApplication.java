82e9ec27283a10da9f7f906d68271a30f5951c45
waelhasnaoui1 <waelhasnaoui006@gmail.com>
2024-07-15 21:12:42 +0100

    intial commit

---
+++ src/main/java/com/example/demojenkins/DemojenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demojenkins/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.example.demojenkins.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class MyController {
+     @GetMapping
+    public String hello(){
+         return "hello from wael app";
+     }
+}
